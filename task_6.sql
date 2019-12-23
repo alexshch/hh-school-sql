d49a18114b66ff08d1ab28453df39ff1079fc65e
alexander scherbakov <asherbakov@avanpost.ru>
2019-12-23 15:27:40 +0300

    task 6 done

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,11 @@
-SELECT cv.cv_id, array_agg(c.specialization_id)
-FROM cv JOIN cv_specialization c ON cv.cv_id = c.cv_id
-LEFT JOIN response r on cv.cv_id = r.cv_id
-LEFT JOIN vacancy v on r.vacancy_id = v.vacancy_id
-LEFT JOIN vacancy_body vb on v.vacancy_body_id = vb.vacancy_body_id
-LEFT JOIN vacancy_body_specialization vbs on v.vacancy_body_id = vbs.vacancy_body_id
-GROUP BY cv.cv_id;
+WITH T1 AS (SELECT cv.cv_id AS cv_id, array_agg(c.specialization_id) AS specs
+FROM cv LEFT JOIN cv_specialization c ON cv.cv_id = c.cv_id
+GROUP BY cv.cv_id)
+SELECT cv_id, cv_spec, vac_specialization_id
+  FROM (SELECT cv_id, cv_spec, vac_specialization_id, ROW_NUMBER() OVER (PARTITION BY cv_id ORDER BY freq DESC) AS rn
+        FROM (SELECT T1.cv_id AS cv_id, T1.specs AS cv_spec, vbs.specialization_id AS vac_specialization_id, COUNT('x') AS freq
+            FROM T1 LEFT JOIN response r ON T1.cv_id = r.cv_id
+            LEFT JOIN vacancy v ON v.vacancy_id = r.vacancy_id
+            LEFT JOIN vacancy_body vb ON vb.vacancy_body_id = v.vacancy_body_id
+            LEFT JOIN vacancy_body_specialization vbs ON vbs.vacancy_body_id = vb.vacancy_body_id
+    GROUP BY T1.cv_id, T1.specs, vbs.specialization_id) spec_freq) ranked_spec_freq WHERE rn = 1;

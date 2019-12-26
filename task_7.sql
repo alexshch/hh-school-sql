ec31709f21618383bb2f70686bc851f998fb53d2
alexander scherbakov <asherbakov@avanpost.ru>
2019-12-26 16:45:37 +0300

    fix task 7

--- task_7.sql
+++ task_7.sql
@@ -1,2 +1,1 @@
--- самый долгий запрос получился в задании 6. После добавления индекса на поле из трех темнокрасных полей одно посветлело
-CREATE INDEX cv_idx_1 ON cv(cv_id);
+CREATE INDEX cv_specialization_idx ON cv_specialization(cv_id);
